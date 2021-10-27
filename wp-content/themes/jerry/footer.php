2664b82d7ae68e49372a6c527f558b8a4d7b84dc
jerrymill <volodymyr.gorbovuy@israelit.pro>
2021-10-27 17:43:44 +0300

    add menu

--- wp-content/themes/jerry/footer.php
+++ wp-content/themes/jerry/footer.php
@@ -1,6 +1,24 @@
         <footer>
             Підвал сайту
             <?php wp_footer(); ?>
+            <?php wp_nav_menu( [
+                'theme_location'  => 'bottom',
+                'menu'            => '',
+                'container'       => 'div',
+                'container_class' => '',
+                'container_id'    => '',
+                'menu_class'      => 'bottom-nav',
+                'menu_id'         => 'bottom-nav',
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu',
+                'before'          => '',
+                'after'           => '',
+                'link_before'     => '',
+                'link_after'      => '',
+                'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'depth'           => 0,
+                'walker'          => '',
+            ] ); ?>
         </footer>
 
     </body>
--- wp-content/themes/jerry/functions.php
+++ wp-content/themes/jerry/functions.php
@@ -5,6 +5,7 @@ add_action( 'after_setup_theme', 'theme_register_nav_menu');
 
 function theme_register_nav_menu() {
 	register_nav_menu( 'primary', 'Primary Menu' );
+    register_nav_menu( 'bottom', 'Bottom Menu' );
 }
 
 function style_theme() {
